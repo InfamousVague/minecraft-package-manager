3c18b0b95cf767fa58ce58d093bd1bf7327fb1cb
Matt Wisniewski <contact.mattdylan@gmail.com>
2017-02-25 22:03:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-const yargs = require('yargs').argv;
-const ProgressBar = require('progress');
-const fs = require('fs');
-const chalk = require('chalk');
+// NPM
+const yargs = require('yargs').argv
+const ProgressBar = require('progress')
+const fs = require('fs')
+const chalk = require('chalk')
 
-const init = require('./tools/init');
-const install = require('./tools/install');
+// Local Tools
+const init = require('./tools/init')
+const install = require('./tools/install')
 
 if (yargs.init) {
-  init();
+  init()
 } else {
+  // Make sure we have the mpm.json file locally
   try {
-    require.resolve(`${process.cwd()}/mpm.json`);
+    require.resolve(`${process.cwd()}/mpm.json`)
   } catch (e) {
-    console.error('No mpm.json found, are you in the right place?');
+    console.error('No mpm.json found, are you in the right place?')
     process.exit();
   }
 
+  // Load up our options
   let opts = {
     install: (yargs.i) ? true : false,
     init: (yargs.init) ? true : false,
@@ -29,21 +33,22 @@ if (yargs.init) {
       `${process.cwd()}/${yargs.p}` : `${process.cwd()}/plugins`,
     package: require(`${process.cwd()}/mpm.json`),
     ticks: 0
-  };
-
-
-  opts.ticks = Object.keys(opts.package.plugins).length;
+  }
+  
+  // Ticks
+  opts.ticks = Object.keys(opts.package.plugins).length
   if (yargs.j) opts.ticks = opts.ticks + 1;
   if (opts.package.configs) {
     opts.ticks = opts.ticks + Object.keys(opts.package.configs).length
   }
 
+  // Yargs args
   if (yargs.a) {
-    let newPackage = opts.package;
-    let name = yargs.a.split('@')[0];
-    let resource = yargs.a.split('@')[1];
-    newPackage.plugins[name] = resource;
-    opts.package = newPackage;
+    let newPackage = opts.package
+    let name = yargs.a.split('@')[0]
+    let resource = yargs.a.split('@')[1]
+    newPackage.plugins[name] = resource
+    opts.package = newPackage
 
     fs.writeFile(`${process.cwd()}/mpm.json`, JSON.stringify(newPackage, null, 2), () => {
       console.log(
@@ -51,22 +56,23 @@ if (yargs.init) {
       );
     });
   }
-
+   
+  // Progress Bar
   opts.bar = new ProgressBar(
-    `${chalk.blue(':plugin → :to')} ${chalk.green(':bar :elapsed')}`
-  , {
+    `${chalk.blue(':plugin → :to')} ${chalk.green(':bar :elapsed')}`, 
+  {
     total: opts.ticks,
     incomplete: '░',
     complete: '█',
     width: 50
-  });
-
+  })
+  
   // If plugins location directory doesn't exist, create it.
   if (!fs.existsSync(opts.location)){
       fs.mkdirSync(opts.location);
   }
 
   if (opts.install) {
-    install(opts, yargs);
+    install(opts, yargs)
   }
 }
